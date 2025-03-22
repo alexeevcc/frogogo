b146a20fd7d232fa42a9f369af297c32fa6f36ff
alexeevcc <snaker5252@gmail.com>
2025-03-22 13:01:55 +0300

    feat: validate required attributes of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :cart_items
+
+  validates :name, :price, presence: true
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,13 +9,13 @@ class ProductTest < ActiveSupport::TestCase
   test "should not create product without name" do
     product = Product.new(price: 1600.00)
     assert_not product.valid?
-    assert_equal ["не может быть пустым"], product.errors[:name]
+    assert_equal ["can't be blank"], product.errors[:name]
   end
 
   test "should not create product without price" do
     product = Product.new(name: "Беспроводная колонка")
     assert_not product.valid?
-    assert_equal ["не может быть пустым"], product.errors[:price]
+    assert_equal ["can't be blank"], product.errors[:price]
   end
 end
 
