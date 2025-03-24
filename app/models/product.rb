c9395f542a43f3dd3094a4436d0b271eae06e457
alexeevcc <snaker5252@gmail.com>
2025-03-24 18:49:50 +0300

    feat: add product images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :cart_items
 
+  has_one_attached :image
+
   validates :name, :price, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,46 +2,35 @@ Product.destroy_all
 Cart.destroy_all
 CartItem.destroy_all
 
-goodyear_speaker = Product.create!(
-  name: "Беспроводная колонка Goodyear Bluetooth Speaker",
-  price: 1600.00,
-  image: "goodyear_speaker.jpg"
-)
+products = [
+  {
+    name: "Беспроводная колонка Goodyear Bluetooth Speaker",
+    price: 1600.00,
+    image_path: Rails.root.join("app/assets/images/product-1.png")
+  },
+  {
+    name: "Женский домашний костюм Sweet Dreams",
+    price: 800.00,
+    image_path: Rails.root.join("app/assets/images/product-2.png")
+  },
+  {
+    name: "Плащ-дождевик SwissOak",
+    price: 800.00,
+    image_path: Rails.root.join("app/assets/images/product-3.png")
+  }
+]
 
-sweet_dreams_suit = Product.create!(
-  name: "Женский домашний костюм Sweet Dreams",
-  price: 800.00,
-  image: "sweet_dreams_suit.jpg"
-)
+products.each do |product_data|
+  product = Product.create(
+    name: product_data[:name],
+    price: product_data[:price]
+  )
 
-swiss_oak_raincoat = Product.create!(
-  name: "Плащ-дождевик SwissOak",
-  price: 800.00,
-  image: "swiss_oak_raincoat.jpg"
-)
-
-cart = Cart.create!(
-  discount: 1000.00,
-  total_price: 3200.00,
-  final_price: 2200.00
-)
-
-CartItem.create!(
-  cart: cart,
-  product: goodyear_speaker,
-  quantity: 1
-)
-
-CartItem.create!(
-  cart: cart,
-  product: sweet_dreams_suit,
-  quantity: 1
-)
-
-CartItem.create!(
-  cart: cart,
-  product: swiss_oak_raincoat,
-  quantity: 2
-)
+  product.image.attach(
+    io: File.open(product_data[:image_path]),
+    filename: File.basename(product_data[:image_path]),
+    content_type: 'image/png'
+  )
+end
 
 puts "Сиды успешно созданы!"
