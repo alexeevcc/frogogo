f410226e121c2af12703f25b473f2bd5d7ed5d40
alexeevcc <snaker5252@gmail.com>
2025-03-28 18:44:30 +0300

    feat: validates for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
 
   has_one_attached :image
 
   validates :name, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :image,
+            content_type: ['image/png', 'image/jpeg', 'image/webp'],
+            size: { less_than: 5.megabytes }
 end
